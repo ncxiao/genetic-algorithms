ecda7ad019f2dd2195389fce8fdb957a8b323eea
Ningchuan Xiao <ncxiao@gmail.com>
2019-06-01 10:13:39 -0400

    ga binary string

--- ga_binary.py
+++ ga_binary.py
@@ -1,7 +1,4 @@
-# this one uses both objs and fitnesses 
-
 from random import random, randint, sample, uniform
-from math import sin, fabs
 
 def flip(p):
     if random() < p:
@@ -10,12 +7,12 @@ def flip(p):
     
 def crossover(s1, s2, prob):
     if not flip(prob):
-        return [s1, s2]
+        return [s1, s2], 0
     n = len(s1)
     x = randint(0, n-1)
     s1x = s1[:x] + s2[x:]
     s2x = s2[:x] + s1[x:]
-    return [s1x, s2x]
+    return [s1x, s2x], x
 
 def mutation(s, prob):
     mutated = ''
@@ -37,10 +34,8 @@ def decode(s):
         return num
     return _decode(s[:3]), _decode(s[3:6]), _decode(s[6:])
 
- # f(x1, x2, x3) = x1 ^ 2 – x2 – x3
 def obj(x):
     return x[0] ** 2 - x[1] - x[2] + 1
-    # return x[0] ** 2 - x[1] + fabs(sin(x[2]))
 
 def obj2fitness(objs):
     low = min(objs)
@@ -68,7 +63,7 @@ def generation(population, objs, fitnesses, pcrossover, pmutation, elitism=True)
     while len(newpop) < len(population):
         p1 = population[select(fitnesses)]
         p2 = population[select(fitnesses)]
-        offspring = crossover(p1, p2, pcrossover)
+        offspring = crossover(p1, p2, pcrossover)[0]
         for s in offspring:
             c = mutation(s, pmutation)
             if len(newpop) < len(population):
@@ -122,6 +117,23 @@ def run_many_GAs(n, param):
     all_best.sort()
     return(all_best[n//2], all_best[0]) # return median and worst
 
+def test():
+    psizes = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+    nruns = [5, 10, 15, 20, 25, 30]
+    pcross = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
+    pmutate = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
+    elitisms = [True, False]
+
+    for n in psizes:
+        for nr in nruns:
+            for pc in pcross:
+                for pm in pmutate:
+                    for e in elitisms:
+                        paramx = Parameters(n, nr, pc, pm, e)
+                        res = run_many_GAs(10, paramx)
+                        print(n, nr, pc, pm, e, res[0], res[1])
+
+# test()
 
 param = Parameters(
     popsize = 10,
@@ -140,24 +152,5 @@ for _ in range(10):
         population, objs, fitnesses = generation(population, objs, fitnesses, 0.9, 0.1, False)
     all_result.append(max(objs))
 
-print(all_result)
-
-print([GA(param)[0] for _ in range(10)])
-
-def test():
-    psizes = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-    nruns = [5, 10, 15, 20, 25, 30]
-    pcross = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
-    pmutate = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
-    elitisms = [True, False]
-
-    for n in psizes:
-        for nr in nruns:
-            for pc in pcross:
-                for pm in pmutate:
-                    for e in elitisms:
-                        paramx = Parameters(n, nr, pc, pm, e)
-                        res = run_many_GAs(10, paramx)
-                        print(n, nr, pc, pm, e, res[0], res[1])
-
-# test()
+print('no elitism  :', all_result)
+print('with elitism:', [GA(param)[0] for _ in range(10)])
