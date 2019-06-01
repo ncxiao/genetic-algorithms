b55f54c88faade37f0f8b62ce9c3306fb8d23fdf
Ningchuan Xiao <ncxiao@gmail.com>
2019-06-01 11:56:27 -0400

    GA class p-median

--- ga_base_pmed.py
+++ ga_base_pmed.py
@@ -1,7 +1,7 @@
 from ga import *
 from random import randint, sample
 
-class GA(GABase):
+class GAPMed(GABase):
     def __init__(self, params, owndata):
         super().__init__(params)
         self.n = owndata['n']
@@ -56,63 +56,62 @@ class GA(GABase):
                     break
         return s
 
+if __name__ == '__main__':
+    owndata = {
+        'n': 8,
+        'distmatrix': [
+            [0, 3, 13, 5, 12, 16, 17, 20],
+            [3, 0, 10, 8, 9, 13, 14, 23],
+            [13, 10, 0, 8, 9, 3, 14, 15],
+            [5, 8, 8, 0, 17, 11, 22, 15],
+            [12, 9, 9, 17, 0, 6, 5, 16],
+            [16, 13, 3, 11, 6, 0, 11, 12],
+            [17, 14, 14, 22, 5, 11, 0, 11],
+            [20, 23, 15, 15, 16, 12, 11, 0]],
+        'p': 2
+    }
 
-owndata = {
-    'n': 8,
-    'distmatrix': [
-        [0, 3, 13, 5, 12, 16, 17, 20],
-        [3, 0, 10, 8, 9, 13, 14, 23],
-        [13, 10, 0, 8, 9, 3, 14, 15],
-        [5, 8, 8, 0, 17, 11, 22, 15],
-        [12, 9, 9, 17, 0, 6, 5, 16],
-        [16, 13, 3, 11, 6, 0, 11, 12],
-        [17, 14, 14, 22, 5, 11, 0, 11],
-        [20, 23, 15, 15, 16, 12, 11, 0]],
-    'p': 2
-}
+    params = Parameters(
+        popsize = 4,
+        numgen = 10,
+        pcrossover = 0.9,
+        pmutation = 0.1,
+        elitism = False,
+        minmax = 'min')
 
-params = Parameters(
-    popsize = 4,
-    numgen = 10,
-    pcrossover = 0.9,
-    pmutation = 0.1,
-    elitism = False,
-    minmax = 'min')
+    myga = GAPMed(params, owndata)
+    res = myga.run()
+    print(res)
 
-myga = GA(params, owndata)
-res = myga.run()
-print(res)
+    # test mutation rates
+    pmutate = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    paramx = [Parameters(4, 10, 0.9, pm, True, 'min') for pm in pmutate]
 
-# test mutation rates
-pmutate = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-paramx = [Parameters(4, 10, 0.9, pm, True, 'min') for pm in pmutate]
+    def multirun(prog, n, best):
+        all_best = []
+        for _ in range(n):
+            res = prog.run()
+            all_best.append(res[0])
+        return sum([1 if res==best else 0 for res in all_best])
 
-def multirun(prog, n, best):
-    all_best = []
-    for _ in range(n):
-        res = prog.run()
-        all_best.append(res[0])
-    return sum([1 if res==best else 0 for res in all_best])
+    res = [multirun(myga, 100, 40) for myga in [GAPMed(par, owndata) for par in paramx]]
 
-res = [multirun(myga, 100, 40) for myga in [GA(par, owndata) for par in paramx]]
+    print('{:15} {}'.format('mutation', 'obj'))
+    for i in range(len(res)):
+        print('{:15} {}'.format(pmutate[i], res[i]))
 
-print('{:15} {}'.format('mutation', 'obj'))
-for i in range(len(res)):
-    print('{:15} {}'.format(pmutate[i], res[i]))
+    def test():
+        pcross = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
+        pmutate = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
+        for pc in pcross:
+            for pm in pmutate:
+                paramx = Parameters(4, 10, pc, pm, True, 'min')
+                myga = GAMed(paramx, owndata)
+                
+                all_best = []
+                for _ in range(1000):
+                    res = myga.run()
+                    all_best.append(res[0])
+                print(pc, pm, sum([1 if res==40 else 0 for res in all_best]))
 
-
-def test():
-    pcross = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
-    pmutate = [0, 0.1, 0.3, 0.5, 0.7, 0.9]
-    for pc in pcross:
-        for pm in pmutate:
-            paramx = Parameters(4, 10, pc, pm, True, 'min')
-            myga = GA(paramx, owndata)
-            
-            all_best = []
-            for _ in range(1000):
-                res = myga.run()
-                all_best.append(res[0])
-            print(pc, pm, sum([1 if res==40 else 0 for res in all_best]))
-
-# test()
+    # test()
